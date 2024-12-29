77b1f2dc2a11104ad5efb62fc2d6483f6bbd993b
matheus1714 <matheussilvamartins1714@gmail.com>
2024-12-28 23:15:57 -0300

    refat: clean code using better design patter

--- src/components/terminal/Terminal.tsx
+++ src/components/terminal/Terminal.tsx
@@ -1,117 +1,84 @@
 import { useState, useEffect, useRef, type ReactNode } from "react";
-import { toggleMarkdownTheme, updateToggleThemeIcon } from "@/scripts/theme";
-import { commandsExceptionsMap, commandsMap, commandsSuccessMap } from "./commands";
+
+import {
+    CommandInvoker,
+    ClearCommand,
+    CVCommand,
+    EchoCommand,
+    EducationCommand,
+    ExperienceCommand,
+    HelpCommand,
+    ProjectsCommand,
+    SkillsCommand,
+    SocialCommand,
+    ThemeCommand,
+    WelcomeCommand,
+    WhoAmICommand
+} from "./commands";
+
 import { transformMarkdown } from "./transform-markdown";
-import { basic } from '@/config/cv.json';
-
-const bashDesign = '[math-term:~$]';
-
-function translateCommandToHTML(command: string) {
-    return (
-        <pre className="whitespace-pre-wrap">
-            { transformMarkdown(command) }
-        </pre>
-    );
-}
-
-const initialStateCommand = [
-    <pre className="flex">
-        <p className="font-bold text-primary font-mono">{bashDesign}</p>&nbsp;
-        <p className="font-mono">welcome</p>
-    </pre>,
-    <br/>,
-    commandsMap.welcome.map(translateCommandToHTML),
-    <br/>,
-];
 
 export function Terminal() {
-    const [command, setCommand] = useState("");
-    const [output, setOutput] = useState<React.ReactNode[]>(initialStateCommand);
+    const bash = '[math-term:~$]';
+
+    const [input, setInput] = useState("welcome");
+    const [output, setOutput] = useState<React.ReactNode[]>([]);
+
     const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-    function appendOutput(command: string, content: ReactNode[]) {
+    const invoker = new CommandInvoker();
+    invoker.registerCommand("whoami", new WhoAmICommand());
+    invoker.registerCommand("experience", new ExperienceCommand());
+    invoker.registerCommand("education", new EducationCommand());
+    invoker.registerCommand("skills", new SkillsCommand());
+    invoker.registerCommand("projects", new ProjectsCommand());
+    invoker.registerCommand("social", new SocialCommand());
+    invoker.registerCommand("cv", new CVCommand());
+    invoker.registerCommand("theme", new ThemeCommand());
+    invoker.registerCommand("help", new HelpCommand());
+    invoker.registerCommand("welcome", new WelcomeCommand());
+    invoker.registerCommand("echo", new EchoCommand());
+    invoker.registerCommand("clear", new ClearCommand());
+
+    const handleCommand = () => {
+        const result = invoker.executeCommand(input);
+        if (input === "clear") {
+            setOutput([]);
+        } else {
+            appendOutput(input, transformMarkdown(result));
+        }
+        setInput("");
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key === "Enter" && input.trim().length > 0) {
+            e.preventDefault();
+            handleCommand();
+        }
+    };
+
+    const generateRandomKey = () => Math.random().toString(36).substr(2, 9) + Date.now();
+
+    function appendOutput(command: string, content: ReactNode[][]) {
         setOutput((prev) => [
             ...prev,
-            <div className="flex">
-                <p className="font-bold text-primary">{bashDesign}</p>&nbsp;<br />
+            <pre className="flex">
+                <p className="font-bold text-primary">{bash}</p>&nbsp;<br />
                 <p>{command}</p>
-            </div>,
+            </pre>,
             <br />,
-            ...content,
+            ...content.map((c, i) => (
+                <pre className="whitespace-pre-wrap" key={i}>
+                    {c}
+                </pre>
+            )),
             <br />,
         ]);
     }
 
-    function handleThemeCommand(command: string) {
-        const themes = ["light", "dark"];
-        const theme = command.replace("theme ", "").trim();
-
-        if (themes.includes(theme)) {
-            document.documentElement.dataset.theme = theme;
-            localStorage.setItem("theme", theme);
-            updateToggleThemeIcon();
-            toggleMarkdownTheme(theme);
-            appendOutput(command, []);
-        } else if(theme === "theme") {
-            appendOutput(command, commandsMap.theme.map(translateCommandToHTML));
-        } else {
-            appendOutput(command, commandsExceptionsMap.themeNotFound.map(translateCommandToHTML));
-        }
-    }
-
-    function handleClearCommand() {
-        setOutput([]);
-    }
-
-    function handleMappedCommand(command: string) {
-        const commandKey = Object.keys(commandsMap).find((cmd) => command.startsWith(cmd));
-        if (commandKey) {
-            appendOutput(command, commandsMap[commandKey].map(translateCommandToHTML));
-        } else {
-            appendOutput(command, commandsExceptionsMap.commandNotFound.map(translateCommandToHTML));
-        }
-    }
-
-    function handleCVCommand(command: string) {
-        const options = ["online", "download"];
-        const option = command.replace("cv ", "").trim();
-
-        if (options.includes(option)) {
-            if(option === "online") {
-                appendOutput(command, commandsSuccessMap.redirecting);
-                window.open(basic.cv_link, "_blank");
-            } else if(option === "download") {
-                const downloadCVLink = `${basic.cv_link}/export?format=pdf`;
-                const anchor = document.createElement("a");
-                anchor.href = downloadCVLink;
-                anchor.download = "";
-                document.body.appendChild(anchor);
-                anchor.click();
-                document.body.removeChild(anchor);
-                
-                appendOutput(command, commandsSuccessMap.downloadCompleted);
-            }
-        } else if(option === "cv") {
-            appendOutput(command, commandsMap.cv.map(translateCommandToHTML));
-        } else {
-            appendOutput(command, commandsExceptionsMap.commandNotFound.map(translateCommandToHTML));
-        }
-    }
-
-    function handleCommand() {
-        if (command.startsWith("theme")) {
-            handleThemeCommand(command);
-        } else if(command.startsWith("cv")) {
-            handleCVCommand(command);
-        } else if (command.startsWith("clear")) {
-            handleClearCommand();
-        } else {
-            handleMappedCommand(command);
-        }
-        setCommand("");
-    }
-
     useEffect(() => {
+        handleCommand();
+
         const focusTextarea = (event: MouseEvent) => {
             if (textareaRef.current && event.target !== textareaRef.current) {
                 textareaRef.current.focus();
@@ -131,23 +98,18 @@ export function Terminal() {
             </pre>
 
             <pre className="flex">
-                <p className="font-bold text-primary font-mono">{bashDesign}</p>&nbsp;
-                <p className="font-mono">{command}</p>
+                <p className="font-bold text-primary font-mono">{bash}</p>&nbsp;
+                <p className="font-mono">{input}</p>
                 <span className="inline-block w-[10px] h-[1.5em] bg-text animate-[blinker_1s_linear_infinite] font-mono"></span>
             </pre>
-            
+
             <textarea
                 ref={textareaRef}
                 autoFocus
                 className="position absolute -left-96"
-                value={command}
-                onChange={(e) => setCommand(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                        e.preventDefault();
-                        handleCommand();
-                    }
-                }}
+                value={input}
+                onChange={(e) => setInput(e.target.value.replace("\n", ""))}
+                onKeyDown={handleKeyDown}
             />
         </div>
     )
--- src/components/terminal/commands.ts
+++ src/components/terminal/commands.ts
@@ -1,75 +1,228 @@
+import { toggleMarkdownTheme, updateToggleThemeIcon } from '@/scripts/theme';
 import { basic, social, experiences, education, projects } from '@/config/cv.json';
 
-export const commandsMap = {
-    welcome:  [
-        "Welcome to my terminal portfolio!",
-        "----",
-        "For a list of available commands, type `help`.",
-    ],
-    help: [
-        "**whoami**       Help with main informations",
-        "**experience**   Professional Experiences",
-        "**education**    Educational background",
-        "**skills**       Current skills",
-        "**projects**     Main projects",
-        "**social**       My main social networks",
-        "**cv**           See CV. It's possible to do a download or see online",
-        "**theme**        Set theme to `light` or `dark`",
-        "**welcome**      See welcome message",
-        "**clear**        Clears everything from the terminal",
-    ],
-    theme: [
-        "Select a theme with as:",
-        "`theme light`  Light Theme",
-        "`theme dark`   Dark Theme",
-    ],
-    whoami: [
-        "Hello!",
-        `I'm ${basic.name}`,
-        basic.summary
-    ],
-    experience: experiences.map((item) => `**${item.title} [${item.sub_title}] ${item.years}**
-        ${item.details}
-        Skills: [${item.skills.map((s) => `**${s.name}**`).join(', ')}]
-        \n`
-    ),
-    education: education.map((item) => `**${item.title} [${item.sub_title}] ${item.years}**
-        ${item.details}
-        \n`
-    ),
-    skills: [`[${basic.skills.map((item) => `**${item.name}**`).join(', ')}]`],
-    projects: projects.map((item) => {
-        const maxTitleLength = Math.max(...projects.map((s) => s.title.length));
-        const padding = " ".repeat(maxTitleLength - item.title.length + 4);
-        return `${item.title}${padding}[${item.link}](${item.link})`
-    }),
-    social: social.map((item) => {
-        const maxTitleLength = Math.max(...social.map((s) => s.title.length));
-        const padding = " ".repeat(maxTitleLength - item.title.length + 4);
-        return `${item.title}${padding}[${item.url}](${item.url})`
-    }),
-    cv: [
-        "Options for this command",
-        "",
-        "`cv download` download my cv local",
-        "`cv online` redirect to online cv"
-    ]
-};
+/* Abstractions */
 
-export const commandsExceptionsMap = {
-    commandNotFound: [
-        "Command not found. Try again with `help`"
-    ],
-    themeNotFound: [
-        "Theme not found. Select `light` or `dark` theme"
-    ],
+interface Command {
+    execute(args: string[]): null | string;
 }
 
-export const commandsSuccessMap = {
-    downloadCompleted: [
-        "Download completed!"
-    ],
-    redirecting: [
-        "Redirecting to the page"
-    ]
+interface SubCommand {
+    execute(args: string[]): null | string;
+}
+
+/* Sub-commands */
+
+class CvHelpSubCommnad implements SubCommand {
+    execute(): string {
+        return [
+            "Options command `cv`:",
+            "`cv help`      -  Show cv commnad docs",
+            "`cv online`    -  Open my online CV",
+            "`cv download`  -  Download my CV",
+        ].join("\n");
+    }
+}
+
+class CVOnlineSubCommand implements SubCommand {
+    execute() {
+        window.open(basic.cv_link, "_blank");
+        return null;
+    }
+}
+
+class CVDownloadSubCommand implements SubCommand {
+    execute(args: string[]) {
+        const link = document.createElement("a");
+        link.href = `${basic.cv_link}/export?format=pdf`;
+        link.download = "";
+        link.click();
+
+        return null;
+    }
+}
+
+/* Commands */
+
+export class WhoAmICommand implements Command {
+    execute() {
+        return [
+            "Hello!",
+            `I'm ${basic.name}`,
+            basic.summary
+        ].join("\n");
+    }
+}
+
+export class ExperienceCommand implements Command {
+    execute() {
+        return experiences.flatMap((item) => [
+            `**${item.title} [${item.sub_title}] ${item.years}**`,
+            "",
+            `${item.details}`,
+            "",
+            `Skills: [${item.skills.map((s) => `**${s.name}**`).join(', ')}]`,
+            "",
+            "----",
+            ""
+        ]).join("\n");
+    }
+}
+
+export class EducationCommand implements Command {
+    execute() {
+        return education.flatMap((item) => [
+            `**${item.title} [${item.sub_title}] ${item.years}**`,
+            "",
+            `${item.details}`,
+            "",
+            "----",
+            "",
+        ]).join("\n");
+    }
+}
+
+export class SkillsCommand implements Command {
+    execute(args: string[]) {
+        return `[${basic.skills.map((item) => `**${item.name}**`).join(', ')}]`;
+    }
+}
+
+export class ProjectsCommand implements Command {
+    execute(args: string[]) {
+        return projects.map((item) => {
+            const maxTitleLength = Math.max(...projects.map((s) => s.title.length));
+            const padding = " ".repeat(maxTitleLength - item.title.length + 4);
+            return `${item.title}${padding}[${item.link}](${item.link})`
+        }).join("\n");
+    }
+}
+
+export class SocialCommand implements Command {
+    execute(args: string[]) {
+        return social.map((item) => {
+            const maxTitleLength = Math.max(...social.map((s) => s.title.length));
+            const padding = " ".repeat(maxTitleLength - item.title.length + 4);
+            return `${item.title}${padding}[${item.url}](${item.url})`
+        }).join("\n");
+    }
+}
+
+export class CVCommand implements Command {
+    private subCommands: Map<string, SubCommand> = new Map();
+
+    constructor() {
+        this.registerSubCommand("help", new CvHelpSubCommnad());
+        this.registerSubCommand("online", new CVOnlineSubCommand());
+        this.registerSubCommand("download", new CVDownloadSubCommand());
+    }
+
+    registerSubCommand(name: string, command: SubCommand) {
+        this.subCommands.set(name, command);
+    }
+
+    execute(args: string[]) {
+        if(args.length === 0) {
+            return "The command `cv` needs arguments. Use `cv help` for more details.";
+        }
+
+        const subCommand = this.subCommands.get(args[0]);
+        if(!subCommand) {
+            return `Subcommand not found '${args[0]}'. Use . Use \`cv help\` for more details.`;
+        }
+
+        return subCommand.execute(args.slice(1));
+    }
+}
+
+export class ThemeCommand implements Command {
+    execute(args: string[]) {
+        const theme = args[0];
+        const options = ["light", "dark"];
+
+        if(args.length === 0) {
+            return [
+                "Select a theme:",
+                options.map((option) => `- ${option}\n`),
+            ].join("\n");
+        }
+
+        if(options.includes(theme)) {
+            document.documentElement.dataset.theme = theme;
+            localStorage.setItem("theme", theme);
+            updateToggleThemeIcon();
+            toggleMarkdownTheme(theme);
+
+            return `Changed to theme ${theme}`;
+        }
+
+        return [
+            "Theme not found. Select valid theme oprions:",
+            options.map((option) => `- ${option}`),
+        ].join("\n");
+    }
+}
+
+export class WelcomeCommand implements Command {
+    execute(args: string[]) {
+        return [
+            "Welcome to my terminal portfolio!",
+            "----",
+            "For a list of available commands, type `help`.",
+        ].join("\n");
+    }
+}
+
+export class HelpCommand implements Command {
+    execute(args: string[]): string {
+        return [
+            "Command Options:",
+            "",
+            "**whoami**       Personal presentation",
+            "**experience**   Professional Experiences",
+            "**education**    Educational background",
+            "**skills**       Current skills",
+            "**projects**     Main projects",
+            "**social**       My main social networks",
+            "**cv**           See CV. It's possible to do a download or see online",
+            "**theme**        Set theme to `light` or `dark`",
+            "**help**         Help with main informations",
+            "**welcome**      See welcome message",
+            "**echo**         Write something on terminal",
+            "**clear**        Clears everything from the terminal",
+        ].join("\n");
+    }
+}
+
+export class EchoCommand implements Command {
+    execute(args: string[]): string {
+        return args.join(" ");
+    }
+}
+
+export class ClearCommand implements Command {
+    execute(args: string[]) {
+        return null;
+    }
+}
+
+/* Invoker */
+
+export class CommandInvoker {
+    private commands: Map<string, Command> = new Map();
+
+    registerCommand(name: string, command: Command) {
+        this.commands.set(name, command);
+    }
+
+    executeCommand(input: string) {
+        const [name, ...args] = input.trim().split(" ");
+        const command = this.commands.get(name);
+
+        if(!command) {
+            return `Command ${input} not found. Use \`help\` to see more options`;
+        }
+
+        return command.execute(args);
+    }
 }
